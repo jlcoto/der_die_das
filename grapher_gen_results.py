9777fab1f5fa07fa006153a9fa31c65cf10cda84
jscoto@gmail.com <Jose@wlan-141-23-107-254.tubit.tu-berlin.de>
2016-06-07 17:57:43 +0200

    First entry gender game

---
+++ grapher_gen_results.py
@@ -0,0 +1,190 @@
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+class Graphs:
+    def __init__(self, data_base):
+        self.data_base = data_base
+    
+    def overall_results_per(self):
+        correct = self.data_base["correct"].sum() / len(self.data_base)
+        wrong = (self.data_base["correct"]==False).sum() / len(self.data_base)
+        results = [correct, wrong]
+        outcome = (0.30, 0.50)
+        fig, ax = plt.subplots()
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        width = 0.15 
+        bars = ax.bar(outcome, results, width, zorder=3)
+        bars[0].set_color('lightseagreen')
+        bars[1].set_color('coral')
+        ax.set_xticks((0.375, 0.575))
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_xticklabels(('Right','Wrong'))
+        fig.suptitle("Percentage Right/Wrong", fontsize=15)
+        plt.show()
+
+    def results_per_gen(self):
+        grouped = (self.data_base["correct"]==True).groupby(self.data_base["article"])
+        correct = grouped.mean()
+        incorrect = 1 - grouped.mean()
+        gender_track = correct.append(incorrect)
+        fig, ax = plt.subplots()
+        ind_der = (1, 1.25)
+        ind_die = (1.75, 2)
+        ind_das = (2.50, 2.75)
+        width = 0.25
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        bars_der = ax.bar(ind_der, gender_track["der"], width)
+        bars_der[0].set_color('lightseagreen')
+        bars_der[1].set_color('coral')
+        bars_die = ax.bar(ind_die, gender_track["die"], width)
+        bars_die[0].set_color('lightseagreen')
+        bars_die[1].set_color('coral')
+        bars_das = ax.bar(ind_das, gender_track["das"], width)
+        bars_das[0].set_color('lightseagreen')
+        bars_das[1].set_color('coral')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_xticks((1.25, 2, 2.75))
+        ax.set_xticklabels(('der','die', 'das'))
+        ax.set_xlim([0.9, 3.1])
+        fig.suptitle("Percentage Right/Wrong by Gender", fontsize=15)
+        plt.show()
+
+    def results_per_date(self):
+        grouped = (self.data_base["correct"]==True).groupby(self.data_base["date"])
+        correct = grouped.mean()
+        wrong = 1 - grouped.mean()
+        data_date = correct.append(wrong)
+        fig, ax = plt.subplots()  
+        x_loc = np.array([0, 0.15])
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        ticks = []
+        dates_played = []
+        for date in pd.unique(data_date.index):
+            width = 0.15
+            x_loc += 0.75
+            bars_date = ax.bar(x_loc, data_date[date], width, zorder=3)
+            bars_date[0].set_color('lightseagreen')
+            bars_date[1].set_color('coral')
+            ticks.append((width+x_loc)[0])
+            dates_played.append(datetime.datetime.strptime(date, '%d-%m-%Y' ).strftime("%d %b"))
+        
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(dates_played)
+        fig.suptitle("Right/Wrong by date", fontsize=15)
+        ax.set_xlim([ax.get_xlim()[0], ax.get_xlim()[1]+0.15])
+        plt.show()
+
+    def wrong_rank(self):
+        wrong_words = self.data_base[self.data_base["correct"]==False]
+        wrong_by_word = wrong_words.groupby(wrong_words["wort"])
+        func = {"correct": "count", "article": lambda x: x.iloc[0]} ## "article": Getting the first value of entry
+        wrong_by_word =  wrong_by_word.agg(func).rename(columns = {"correct": "wrong"})
+        wrong_by_word = wrong_by_word.reset_index()
+        wrong_ones_sort = wrong_by_word.sort_values(['wrong', 'wort'], ascending = [0, 0])
+        first_twenty_wrong = wrong_ones_sort[0:20]
+        col_num = np.arange(first_twenty_wrong.shape[0])
+        fig, ax = plt.subplots(figsize=(8, 6))
+        width = 0.65
+        bars_worng = ax.bar(col_num, first_twenty_wrong["wrong"], width, color= 'powderblue'
+                           , linewidth=0.5)
+        ax.set_xlim([-0.5, ax.get_xlim()[1]])
+        ax.set_ylim([0, ax.get_ylim()[1] +1])
+        ax.set_title('Ranking of wrong words', fontsize=15)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_yticks(np.arange(first_twenty_wrong.iloc[-1]["wrong"], first_twenty_wrong.iloc[0]["wrong"]+2, 1.0))
+        ax.set_xticks(col_num + 0.325)
+        ax.set_xticklabels(list(first_twenty_wrong['wort']))
+        plt.setp(ax.get_xticklabels(), rotation=90)
+        
+        for article in range(len(list(first_twenty_wrong['article']))):
+            ax.text(article + 0.4, 0.15 , list(first_twenty_wrong['article'])[article], rotation= 90,
+                   horizontalalignment='center',
+                verticalalignment='center', fontsize=12) 
+        plt.tight_layout()
+        plt.show()
+
+    def daily_stats(self):
+        grouped = (self.data_base["correct"]==True).groupby(self.data_base["date"])
+        correct = grouped.mean().reset_index()
+        correct["wrong"] =  1 - correct["correct"]
+        fig, ax = plt.subplots()
+        dates = [datetime.datetime.strptime(date, '%d-%m-%Y' ).date() for date in correct["date"] ]
+        left_limit = (datetime.datetime.strptime(pd.unique(self.data_base["date"])[0], '%d-%m-%Y' )  - 
+                      datetime.timedelta(days= 1)).date() 
+        right_limit = (datetime.datetime.strptime(pd.unique(self.data_base["date"])[-1], '%d-%m-%Y' )  
+                       + datetime.timedelta(days=1)).date()
+        ax.plot(dates, correct['correct'], marker = '.', color='lightseagreen',
+                                           ms = 15, lw = 2, linestyle= '-' , label="correct" )
+        ax.plot(dates, correct['wrong'], color='coral', marker = '.',
+                                           ms = 15, lw = 2, linestyle= '-', label="wrong"  )
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        ax.set_xticks((dates))
+        ax.set_xlim([left_limit, right_limit])
+        ax.set_ylim([0., 1.])
+        ax.legend(loc='upper right').get_frame().set_alpha(0.3)
+        ax.set_title('Daily Stats', fontsize=15)
+        ax.set_xticklabels([date.strftime("%d %b") for date in dates])
+        plt.show()
+
+    def daily_stats_per_gender(self):
+        grouped = (self.data_base["correct"]==True).groupby([self.data_base["date"], self.data_base["article"]])
+        correct_gender = grouped.mean().reset_index()
+        correct_gender["wrong"] = 1 - correct_gender["correct"] 
+        correct_gender = correct_gender.set_index(["date", "article"]).unstack().reset_index()
+        dates = [datetime.datetime.strptime(date, '%d-%m-%Y' ).date() 
+                 for date in correct_gender["date"] ]
+        left_limit = (datetime.datetime.strptime(pd.unique(self.data_base["date"])[0], '%d-%m-%Y' )  - 
+                      datetime.timedelta(days= 1)).date() 
+        right_limit = (datetime.datetime.strptime(pd.unique(self.data_base["date"])[-1], '%d-%m-%Y' )  
+                       + datetime.timedelta(days=1)).date()
+        fig= plt.figure()
+        ax = fig.add_axes([0.1, 0.2, 0.85, 0.70])
+        lab_desc, lab_chars = [], []
+        gen_spec = {"der":"#6191C5" , "die":"#D56054" , "das": "#69B17D"}
+        for gender, color_gen in gen_spec.items():
+            ax.plot(dates, correct_gender["correct"][gender], color= color_gen, marker = '.', 
+                    ms = 12, lw = 2, linestyle= '-' )
+            ax.plot(dates, correct_gender["wrong"][gender], color= color_gen, marker = '.', 
+                    ms = 12, lw = 2, linestyle= '--')
+            leg_char_corr = plt.Line2D((0,1),(0,0), color=color_gen, marker='.', linestyle='-')
+            leg_char_wrong = plt.Line2D((0,1),(0,0), color=color_gen, marker='.', linestyle='--')
+            lab_chars.extend([leg_char_corr, leg_char_wrong])
+            lab_desc.extend([gender + " right", gender + " wrong"])
+        
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.grid(axis="y", zorder=0, color="#9698A1")
+        ax.set_xticks((dates))
+        ax.set_xlim([left_limit, right_limit])
+        ax.set_ylim([0., 1.])
+        ax.set_title('Daily Stats per gender', fontsize=15)
+        ax.set_xticklabels([date.strftime("%d %b") for date in dates])
+        ax.legend(lab_chars, lab_desc, loc='upper center', 
+                  bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=3)
+        plt.show()
+        
+
+graph_test = Graphs(pd.read_pickle("results"))
+graph_test.results_per_date()
---
+++ noun_game.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import random
+import datetime
+import os.path
+import time
+
+class Gender_guess:
+    """"Implements the main game. A word in German is chosen and the user is asked for the gender."""
+
+    def __init__(self, csvfile):
+        """Takes csv values that contains nouns and generates a pandas data frame to collect results."""
+        self.noun_list = csvfile
+        self.data_base = pd.DataFrame(index=range(0),
+                                      columns=['date', 'time', 'input','article', 'wort', 'word', 'correct'])
+        
+    def file_to_data_frame(self, csvfile):
+        """Passes csv file with Vocabulary to pandas data frame."""
+        data_nouns = pd.read_csv('final_data.csv', delimiter= ",", index_col=0)
+        return data_nouns
+    
+    def pick_word(self, data_frame):
+        """Randomly pick an index to select word to be tested."""
+        random_pick = random.randint(0, len(data_frame.index) -1)
+        return random_pick
+
+    def eng_ger_word(self, random_pick, data_frame):
+        """Select the words in German and English according to the random selection."""
+        german = data_frame.ix[random_pick]["Wort"]
+        english = data_frame.ix[random_pick]["Word"]
+        return german, english
+    
+    def print_words(self, german, english):
+        """Prints the set of instructions and words for the user."""
+        german_spacing = int(len(german)/2)
+        english_spacing = int(len(english)/2)
+        total_spacing = (german_spacing + english_spacing)*2 + len("Deutsch"+"English") + 2
+        print("{0}{1}{2}|| {3}{4}{5} \n {6} ".format
+              (" "*german_spacing  ,"Deutsch", " "*german_spacing ," "*english_spacing
+               ,"English" , " "*english_spacing, '='* total_spacing))
+        print(" {0}{1}{2}\n\n".format(german," "*(german_spacing + 4) , english))
+ 
+    def take_input(self):
+        """Takes input of user. Prechecks input values."""
+        gender = input("Der, die oder das? ... \n\n").lower()
+        while gender not in ["der", "die", "das"]:
+            print("\n\nPlease, make sure to enter a valid gender.")
+            gender = input("Der, die oder das? ... \n\n").lower()
+        return gender
+    
+    def print_input(self, gender_input, german_word):
+        """Prints user's input.""" 
+        print("\n\n{} {}".format(gender_input , german_word))
+        
+    def word_gender(self, random_pick, data_frame):
+        """Chooses the article corresponding to word randomly chosen."""
+        german_gen = data_frame.ix[random_pick]["Article"]
+        return german_gen.strip()  
+    
+    def checker(self, german_gender, gender_input):
+        """Checks if gender is right."""
+        if german_gender == gender_input:
+            print("Das stimmt! :)\n\n")
+            return True
+        else:
+            print("Das ist nicht richtig :( --> {} \n\n ".format(german_gender))
+            return False
+    
+    def gen_data_base(self, gender_input, german_gender, german_wort, english_word, correct):
+        """Updates previously generated data base with results from tests."""
+        to_add = pd.DataFrame({'date': [datetime.date.today().strftime("%d-%m-%Y")], 'time': [datetime.datetime.now().time().strftime("%H:%M:%S")], 
+                               'input': [gender_input],'article': [german_gender], 
+                               'wort': [german_wort], 'word': [english_word], 'correct': [correct]})
+        self.data_base = self.data_base.append(to_add, ignore_index=True)
+    
+    def run_program(self, number_of_words):
+        """Main logic of program. Stores results to a pandas data frame for time series tracking."""
+        data = self.file_to_data_frame('nouns_german')
+        for round in range(number_of_words):
+            time.sleep(1)
+            ran_pick = self.pick_word(data)
+            german = self.eng_ger_word(ran_pick, data)[0]
+            english = self.eng_ger_word(ran_pick, data)[1]
+            german_gender = self.word_gender(ran_pick, data)
+            self.print_words(german, english)
+            gender_input = self.take_input()
+            self.print_input(gender_input, german)
+            correct = self.checker(german_gender, gender_input)
+            self.gen_data_base(gender_input, german_gender, german, english, correct)
+        if os.path.isfile("results"):
+            previous_data = pd.read_pickle("results")
+            previous_data = previous_data.append(self.data_base, ignore_index=True)
+            previous_data.to_pickle("results")
+        else: 
+            self.data_base.to_pickle("results")
+
+
+prueba_1 = Gender_guess('final_data.csv')
+prueba_1.run_program(20)
